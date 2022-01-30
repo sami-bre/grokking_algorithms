468f0ac649796731650140088e0ca5ae4d80876d
Samuel Birhanu <samuelbirhanu121@gmail.com>
2022-01-30 16:33:53 +0300

    adding two files: memoized and unmemoized fibonnachi functions to see how fast they are under the timer

---
+++ fibonnachi.py
@@ -0,0 +1,15 @@
+from codetimer import Timer
+def fib(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+
+print("\nUnmemoised recursion")
+with Timer():
+    print(fib(35))
+
