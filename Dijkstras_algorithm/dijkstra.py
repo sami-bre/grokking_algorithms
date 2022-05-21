0b0e765d1406c0d3c0a2c1298002507da20ff4ed
Samuel Birhanu <samuelbirhanu121@gmail.com>
2022-05-22 00:21:02 +0300

    added some documentation to djikstra.py

--- Dijkstras_algorithm/dijkstra.py
+++ Dijkstras_algorithm/dijkstra.py
@@ -1,3 +1,8 @@
+"""Dijkstra's algorithm is used to find a the single sourse shortest path between to nodes
+on a DAG (Directed acyclic graph). It is not used if the graph contains negative weights.
+for graphs with negative weights, use the Bellman-ford algorithm. for unweighted graphs, use
+BFS (breadth first search)"""
+
 # make the graph
 graph = dict()
 graph["a"] = [("b", 5), ('c', 2)]
